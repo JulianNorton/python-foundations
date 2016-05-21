4cfdecdffc4b7b269dfce51a4ea011add94ef820
Julian Norton <Julian@JulianNorton.com>
2016-05-21 10:53:19 -0400

    whitespace removal working correctly

--- check_profanity.py
+++ check_profanity.py
@@ -1,22 +1,13 @@
 import urllib.request
 
-# easier see output delination
-for i in range(1,5):
-    print('')
-
 def read_text():
     quotes = open('movie_quotes.txt')
     text_to_check = quotes.read()
     quotes.close()
     # print(text)
     
-    # need to replace spaces or it returns "HTTP Error 400: Bad Request"
-    # regular expression? needed to remove all white space.
-    # http://stackoverflow.com/questions/11475885/python-replace-regex/11475905#11475905
-    # http://stackoverflow.com/questions/8270092/python-remove-all-whitespace-in-a-string
-    text_to_check = text_to_check.replace(' ','%20')
-    text_to_check = text_to_check.replace('\n','%20')
-
+    # need to filter out bad whitespace or it returns "HTTP Error 400: Bad Request"
+    text_to_check = ''.join(text_to_check.split())
     # text_to_check = 'shit  '
     check_profanity(text_to_check)
 
@@ -25,11 +16,9 @@ def check_profanity(text_to_check):
     
     url_to_check = 'http://www.wdylike.appspot.com/?q=' + text_to_check
     connection = urllib.request.urlopen(url_to_check)
-    # print(connection)
     output = connection.read()
-    print(output)
     print(url_to_check)
-    print(url_to_check)
+    print('\n', output, '\n')
     connection.close()
 
 read_text()
