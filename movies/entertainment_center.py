29aa879a5a65b846b8b8eb40c0504887850a925d
Julian Norton <Julian@JulianNorton.com>
2016-05-22 18:41:42 -0400

    creating media class and initializing an instance.

---
+++ movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie('toy story',
+                        'Toys come to life',
+                        'https://en.wikipedia.org/wiki/File:Toy_Story.jpg'
+                        ,'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+
+print(toy_story.storyline)
